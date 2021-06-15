c10c896da5086285c00a1871d62021967e59efb7
saket <saketj420@gmail.com>
2021-06-16 00:43:32 +0530

    Getting data by giving query parameter

--- src/main/java/com/bl/springExample/controller/HelloWorldController.java
+++ src/main/java/com/bl/springExample/controller/HelloWorldController.java
@@ -1,15 +1,23 @@
 package com.bl.springExample.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/helloController")
 public class HelloWorldController {
-//public static void main(String[] args) {
+
 	@RequestMapping( method = RequestMethod.GET,value = {"/home"})
 	public String sayHello() {
 		return "HELLO WORLD";
-}
+    }
+	
+	@RequestMapping (value = {"/query"} , method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name") String name ) {
+		return "HELLO  " + name + " !! ";
+	}
+	
 }
